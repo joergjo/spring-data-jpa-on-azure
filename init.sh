9e05975493dc46b3a8fb071fe57e9c08af4306d2
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2020-05-11 10:20:27 +0200

    Updated App Service Dockerfile.

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Set up sshd
-echo "Starting SSH ..."
+echo "Starting SSH..."
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 ssh-keygen -A -q
 /usr/sbin/sshd
 
 # Run Spring Boot app
-java -cp /app:/app/lib/* com.microsoft.azure.samples.spring.Application
+java -javaagent:./applicationinsights-agent-3.0.0.jar -jar spring-data-jpa-on-azure.jar
